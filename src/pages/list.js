63c71073be3051ddf58da62bde94b38fe4c20e5e
Wojciech-Mazurowski-digimuth <wojci123@gmail.com>
2023-05-31 14:27:26 +0200

    pages

--- src/pages/list.js
+++ src/pages/list.js
@@ -1,7 +1,23 @@
+import {useEffect, useState} from "react";
 
 
 export default function List(){
+
+    const [userNames, setUserNames] = useState();
+
+    useEffect(()=>{
+        refreshUserList();
+    },[])
+
+    userNames?.forEach(user => console.log(user));
+
+    const refreshUserList = async () => {
+        const response = await fetch(process.env.REACT_APP_API_URL+ "/getAllSubscribers");
+        const jsonData = await response.json();
+        setUserNames(jsonData);
+    }
+
     return <div>
-        Test
+        {userNames?.map(user => <div>{user.endpoint}</div>)}
     </div>
 }
--- src/pages/sendNotification.js
+++ src/pages/sendNotification.js
@@ -1,8 +1,52 @@
-
+import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
 
 export default function SendNotification(){
 
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+
     return <div>
         TUTAJ SEND NOTIFICATION
+        <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item
+                style={{color:"white"}}
+                label={<label style={{ color: "white" }}>Tytuł</label>}
+                name="title"
+                rules={[{ required: true, message: 'tytuł nie może być pusty' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label={<label style={{ color: "white" }}>Wiadomość</label>}
+                name="body"
+                rules={[{ required: true, message: 'wiadomość nie może być pusta' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                    Wyślij
+                </Button>
+            </Form.Item>
+        </Form>
+
     </div>
 }

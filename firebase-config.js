803aa141c7078da2ebb4ca25df379ed269ed12cc
mateusz-stelmasiak <46268673+mateusz-stelmasiak@users.noreply.github.com>
2023-09-12 15:55:38 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,7 @@ import {get, getDatabase, orderByChild, orderByKey, query, ref} from "firebase/d
 // Your web app's Firebase configuration
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAYR75On_Y2oVJWX1Z-2oXNi07Rw_dvRNE",
-    authDomain: "empushnotifications.firebaseapp.com",
-    databaseURL: "https://empushnotifications-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "empushnotifications",
-    storageBucket: "empushnotifications.appspot.com",
-    messagingSenderId: "175860235754",
-    appId: "1:175860235754:web:847a13037a2c996b778693"
+   
 };
 
 
